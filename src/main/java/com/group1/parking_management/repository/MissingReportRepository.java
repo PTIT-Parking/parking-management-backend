aa71eba7cfcf10c7b8ca01d8f5ffe1f217aafd09
Lezh1n <hoangphilong1208@gmail.com>
2025-03-18 23:09:27 +0700

    complete payment management

--- src/main/java/com/group1/parking_management/repository/MissingReportRepository.java
+++ src/main/java/com/group1/parking_management/repository/MissingReportRepository.java
@@ -1,5 +1,7 @@
 package com.group1.parking_management.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.group1.parking_management.entity.MissingReport;
 
 @Repository
 public interface MissingReportRepository extends JpaRepository<MissingReport, String> {
-
+    List<MissingReport> findAllByOrderByCreateAtDesc();
 }
---
+++ src/main/java/com/group1/parking_management/service/PaymentService.java
@@ -0,0 +1,12 @@
+package com.group1.parking_management.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.group1.parking_management.dto.response.PaymentResponse;
+
+@Service
+public interface PaymentService {
+    public List<PaymentResponse> getAllPayment(); 
+}
