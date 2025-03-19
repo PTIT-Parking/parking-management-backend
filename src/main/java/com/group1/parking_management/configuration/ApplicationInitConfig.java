620dc0b3d1ae056b7a303307c41932f2f9607a91
Lezh1n <hoangphilong1208@gmail.com>
2025-03-19 21:35:10 +0700

    complete statistic feature

--- src/main/java/com/group1/parking_management/configuration/ApplicationInitConfig.java
+++ src/main/java/com/group1/parking_management/configuration/ApplicationInitConfig.java
@@ -1,5 +1,9 @@
 package com.group1.parking_management.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +11,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.group1.parking_management.common.Role;
 import com.group1.parking_management.entity.Account;
+import com.group1.parking_management.entity.ParkingCard;
+import com.group1.parking_management.entity.Price;
+import com.group1.parking_management.entity.VehicleType;
 import com.group1.parking_management.repository.AccountRepository;
+import com.group1.parking_management.repository.ParkingCardRepository;
+import com.group1.parking_management.repository.PriceRepository;
+import com.group1.parking_management.repository.VehicleTypeRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +26,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class ApplicationInitConfig {
+
     private final PasswordEncoder passwordEncoder;
+    private final VehicleTypeRepository vehicleTypeRepository;
+    private final PriceRepository priceRepository;
+    private final ParkingCardRepository parkingCardRepository; 
 
     private static final String ADMIN_USERNAME = "admin";
     private static final String ADMIN_PASSWORD = "admin";
@@ -34,7 +48,36 @@ public class ApplicationInitConfig {
                 accountRepository.save(account);
                 log.warn("admin user has been created with default password: admin, please change it");
             }
+
+            if (parkingCardRepository.count() == 0) {
+                List<ParkingCard> listCard = new ArrayList<>();
+                for (int i = 0; i <= 1000; i++) {
+                    listCard.add(new ParkingCard(i));
+                }
+                parkingCardRepository.saveAll(listCard);
+                log.info("Parking Cards initialized");
+            }
+            
+            if (vehicleTypeRepository.count() == 0) {
+                String bikeId = UUID.randomUUID().toString();
+                String motorId = UUID.randomUUID().toString();
+                String scooterId = UUID.randomUUID().toString();
+                VehicleType bike = new VehicleType(bikeId, "Bicycle");
+                VehicleType motorbike =  new VehicleType(motorId, "Motorbike");
+                VehicleType scooter =  new VehicleType(scooterId, "Scooter");
+
+                vehicleTypeRepository.saveAll(List.of(bike, motorbike, scooter));
+                
+                priceRepository.saveAll(List.of(
+                    new Price(bikeId, bike, 1000, 2000, 25000),
+                    new Price(motorId, motorbike, 3000, 4000, 120000),
+                    new Price(scooterId, scooter, 4000, 5000, 150000)
+                ));
+
+                log.info("Vehicle Type ");
+            }
+
             log.info("Application initialization completed.....");
-        };
+        };   
     }
 }
--- src/main/java/com/group1/parking_management/repository/ParkingRecordHistoryRepository.java
+++ src/main/java/com/group1/parking_management/repository/ParkingRecordHistoryRepository.java
@@ -1,11 +1,16 @@
 package com.group1.parking_management.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.group1.parking_management.entity.ParkingRecordHistory;
 
 @Repository
 public interface ParkingRecordHistoryRepository extends JpaRepository<ParkingRecordHistory, String> {
-
+    @Query("SELECT COUNT(p) FROM ParkingRecordHistory p WHERE p.entryTime BETWEEN :startDate AND :endDate")
+    long sumVehicleBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
