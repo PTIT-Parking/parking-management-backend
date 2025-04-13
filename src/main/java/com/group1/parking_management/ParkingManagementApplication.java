807182744a9959c051804576de0450aba04a3900
Lezh1n <hoangphilong1208@gmail.com>
2025-04-13 15:52:20 +0700

    add @EnableScheduling

--- src/main/java/com/group1/parking_management/ParkingManagementApplication.java
+++ src/main/java/com/group1/parking_management/ParkingManagementApplication.java
@@ -2,8 +2,10 @@ package com.group1.parking_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ParkingManagementApplication {
 
 	public static void main(String[] args) {
