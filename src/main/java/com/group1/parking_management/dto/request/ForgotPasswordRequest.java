28ce542e038dd40436aacac45f474689ee417cd3
Lezh1n <hoangphilong1208@gmail.com>
2025-04-21 23:26:52 +0700

    dto forgot password request

---
+++ src/main/java/com/group1/parking_management/dto/request/ForgotPasswordRequest.java
@@ -0,0 +1,21 @@
+package com.group1.parking_management.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ForgotPasswordRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+}
