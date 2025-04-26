213092a61d0c3df7cd793fac1c6a3066e1fff6af
Lezh1n <hoangphilong1208@gmail.com>
2025-04-26 21:03:01 +0700

    refactor init data

--- src/main/resources/db/init/02-data.sql
+++ src/main/resources/db/init/02-data.sql
@@ -1,50 +1,97 @@
--- Thêm tài khoản admin
-INSERT INTO account (account_id, username, password, role)
-VALUES (UUID(), 'admin', '$2a$10$riOsq0JyAqDzFCXShMPvxeqILBKQnYdZik/ZYpeeEEodkyCMkf6Pq', 'ADMIN')
-ON DUPLICATE KEY UPDATE username = username;
+-- Init Admin account
+INSERT INTO
+    account (account_id, username, password, role)
+VALUES
+    (
+        UUID (),
+        'admin',
+        '$2a$10$riOsq0JyAqDzFCXShMPvxeqILBKQnYdZik/ZYpeeEEodkyCMkf6Pq',
+        'ADMIN'
+    ) ON DUPLICATE KEY
+UPDATE username = username;
+
+-- Add admin's email
+INSERT INTO
+    admin (account_id, email)
+SELECT
+    account_id,
+    'ptitparking.adm@gmail.com'
+FROM
+    account
+WHERE
+    username = 'admin' ON DUPLICATE KEY
+UPDATE email = 'ptitparking.adm@gmail.com';
 
 -- Thêm các loại phương tiện
-INSERT INTO vehicle_type (id, name)
-VALUES 
-    (UUID(), 'Bicycle'),
-    (UUID(), 'Motorbike'),
-    (UUID(), 'Scooter')
-ON DUPLICATE KEY UPDATE name = name;
+INSERT INTO
+    vehicle_type (id, name)
+VALUES
+    (UUID (), 'Bicycle'),
+    (UUID (), 'Motorbike'),
+    (UUID (), 'Scooter') ON DUPLICATE KEY
+UPDATE name = name;
 
 -- Thêm giá cho các loại phương tiện
-INSERT INTO price (type_id, day_price, night_price, monthly_price)
-SELECT vt.id, 1000, 2000, 25000
-FROM vehicle_type vt
-WHERE vt.name = 'Bicycle'
-ON DUPLICATE KEY UPDATE day_price = 1000, night_price = 2000, monthly_price = 25000;
+INSERT INTO
+    price (type_id, day_price, night_price, monthly_price)
+SELECT
+    vt.id,
+    1000,
+    2000,
+    25000
+FROM
+    vehicle_type vt
+WHERE
+    vt.name = 'Bicycle' ON DUPLICATE KEY
+UPDATE day_price = 1000,
+night_price = 2000,
+monthly_price = 25000;
 
-INSERT INTO price (type_id, day_price, night_price, monthly_price)
-SELECT vt.id, 3000, 4000, 120000
-FROM vehicle_type vt
-WHERE vt.name = 'Motorbike'
-ON DUPLICATE KEY UPDATE day_price = 3000, night_price = 4000, monthly_price = 120000;
+INSERT INTO
+    price (type_id, day_price, night_price, monthly_price)
+SELECT
+    vt.id,
+    3000,
+    4000,
+    120000
+FROM
+    vehicle_type vt
+WHERE
+    vt.name = 'Motorbike' ON DUPLICATE KEY
+UPDATE day_price = 3000,
+night_price = 4000,
+monthly_price = 120000;
 
-INSERT INTO price (type_id, day_price, night_price, monthly_price)
-SELECT vt.id, 4000, 5000, 150000
-FROM vehicle_type vt
-WHERE vt.name = 'Scooter'
-ON DUPLICATE KEY UPDATE day_price = 4000, night_price = 5000, monthly_price = 150000;
+INSERT INTO
+    price (type_id, day_price, night_price, monthly_price)
+SELECT
+    vt.id,
+    4000,
+    5000,
+    150000
+FROM
+    vehicle_type vt
+WHERE
+    vt.name = 'Scooter' ON DUPLICATE KEY
+UPDATE day_price = 4000,
+night_price = 5000,
+monthly_price = 150000;
 
 DROP PROCEDURE IF EXISTS create_parking_cards;
 
-DELIMITER //
+DELIMITER / / CREATE PROCEDURE create_parking_cards () BEGIN DECLARE i INT DEFAULT 1;
 
-CREATE PROCEDURE create_parking_cards()
-BEGIN
-    DECLARE i INT DEFAULT 1;
-    
-    WHILE i <= 999 DO
-        INSERT IGNORE INTO parking_card (card_id) VALUES (i);
-        SET i = i + 1;
-    END WHILE;
-END //
+WHILE i <= 999 DO INSERT IGNORE INTO parking_card (card_id)
+VALUES
+    (i);
 
-DELIMITER ;
+SET
+    i = i + 1;
 
-CALL create_parking_cards();
+END WHILE;
+
+END / / DELIMITER;
+
+CALL create_parking_cards ();
+
 DROP PROCEDURE IF EXISTS create_parking_cards;
