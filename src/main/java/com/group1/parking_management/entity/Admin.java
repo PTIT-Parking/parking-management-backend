bc6d4fc90f2fdb5140036e08837f408e43a80b2b
Lezh1n <hoangphilong1208@gmail.com>
2025-04-26 20:10:53 +0700

    create admin entity

--- src/main/java/com/group1/parking_management/entity/Admin.java
+++ src/main/java/com/group1/parking_management/entity/Admin.java
@@ -1,11 +1,23 @@
 package com.group1.parking_management.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "admin")
 public class Admin {
     @Id
     @Column(name = "account_id")
