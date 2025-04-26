141fb0c5de949e13aae55c4f7b0d646585aced40
Lezh1n <hoangphilong1208@gmail.com>
2025-04-26 20:11:19 +0700

    update schema for admin entity

--- src/main/resources/db/init/01-schema.sql
+++ src/main/resources/db/init/01-schema.sql
@@ -33,7 +33,7 @@ CREATE TABLE
   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE
-  IF NOT EXISTS `admin_info` (
+  IF NOT EXISTS `admin` (
     `account_id` varchar(36) NOT NULL,
     `email` varchar(100) NOT NULL,
     PRIMARY KEY (`account_id`),
